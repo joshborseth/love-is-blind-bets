3f86ad456f8bc79fe534fb1618ff89deba3fc04e
joshborseth <joshuaborseth@gmail.com>
2024-09-22 18:46:11 -0700

    made some changes to the app so that things work

--- sst.config.ts
+++ sst.config.ts
@@ -4,7 +4,7 @@
 export default $config({
 	app(input) {
 		return {
-			name: 'love-is-blind',
+			name: 'love-is-blind-betting',
 			removal: input?.stage === 'production' ? 'retain' : 'remove',
 			home: 'aws',
 			providers: {
