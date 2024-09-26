be5210b5eb16e30af7b991337fa4f56be7b944d3
joshborseth <joshuaborseth@gmail.com>
2024-09-25 19:35:46 -0700

    start on dashboard page

--- src/routes/app/+page.server.ts
+++ src/routes/app/+page.server.ts
@@ -1,9 +1,16 @@
 import { db } from '~/db';
-export const load = async () => {
-	const maleContestants = await db.query.maleContestants.findMany();
-	const femaleContestants = await db.query.femaleContestants.findMany();
-	return {
-		maleContestants,
-		femaleContestants
-	};
+
+export const load = async (opts) => {
+	const matches = await db.query.matches.findMany({
+		where: (matches, { eq }) => eq(matches.userId, opts.locals.session.userId)
+	});
+
+	if (!matches.length) {
+		const maleContestants = await db.query.maleContestants.findMany();
+		const femaleContestants = await db.query.femaleContestants.findMany();
+
+		return { maleContestants, femaleContestants };
+	}
+
+	return { matches: matches.length ? matches : null };
 };
--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -1,10 +1,44 @@
 <script lang="ts">
+	import { ContestantCard } from '@/components/custom/ContestantCard';
+
+	export let data;
 </script>
 
 <div class="flex flex-col gap-2">
 	<h1 class="text-3xl font-bold">Dashboard</h1>
 	<p class="text-muted-foreground font-light">
-		Welcome to your dashboard! This is where you can view your selected matches.
+		{#if !data.matches}
+			Get started below!
+		{:else}
+			This is where you can view your selected matches.
+		{/if}
 	</p>
 </div>
-<section class="grid md:grid-cols-2 grid-cols-1 gap-4 p-4 bg-background"></section>
+{#if data.maleContestants && data.femaleContestants}
+	<section class="grid md:grid-cols-2 grid-cols-1 gap-4 py-10 bg-background max-w-4xl w-full">
+		<div class="flex flex-col gap-4">
+			{#each data.maleContestants as item}
+				<ContestantCard
+					name={item.name}
+					job={item.job}
+					gender="Male"
+					imageUrl={item.imageUrl}
+					age={Number(item.age)}
+					desc={item.description}
+				/>
+			{/each}
+		</div>
+		<div class="flex flex-col gap-4">
+			{#each data.femaleContestants as item}
+				<ContestantCard
+					name={item.name}
+					job={item.job}
+					gender="Female"
+					imageUrl={item.imageUrl}
+					age={Number(item.age)}
+					desc={item.description}
+				/>
+			{/each}
+		</div>
+	</section>
+{/if}
