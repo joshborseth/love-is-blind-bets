196bcbe3818c916197cf477a061fb1ce097d702a
joshborseth <joshuaborseth@gmail.com>
2024-09-29 23:05:07 -0700

    more work to marriage ui

--- src/routes/app/marriages/+page.svelte
+++ src/routes/app/marriages/+page.svelte
@@ -15,23 +15,37 @@
 		</p>
 		<p class="font-bold">You may choose {MAX_GEUSSES} couples.</p>
 	</div>
-	<section class="flex gap-2 flex-wrap">
+	<section class="grid grid-cols-3 gap-10">
 		{#each data.matches as match}
 			<Card.Root>
 				<Card.Header class="flex flex-col gap-4">
-					<div>
-						<div>
-							<Card.Title>{match.maleContestant.name}</Card.Title>
-							<Badge class={cn('bg-male hover:bg-male h-5')}>Male</Badge>
+					<div class="flex justify-start gap-10">
+						<img
+							src={match.maleContestant.headShotUrl}
+							alt={match.maleContestant.name}
+							class="rounded-md object-cover h-12 w-12"
+						/>
+						<div class="flex gap-2 flex-col">
+							<div class="flex gap-2">
+								<Card.Title>{match.maleContestant.name}</Card.Title>
+								<Badge class={cn('bg-male hover:bg-male h-5')}>Male</Badge>
+							</div>
+							<Card.Description>{match.maleContestant.age} years old</Card.Description>
 						</div>
-						<Card.Description>{match.maleContestant.age} years old</Card.Description>
 					</div>
-					<div>
-						<Card.Title>{match.femaleContestant.name}</Card.Title>
-						<Badge class={cn('bg-female hover:bg-female h-5')}>
-							{gender}
-						</Badge>
-						<Card.Description>{match.femaleContestant.age} years old</Card.Description>
+					<div class="flex justify-start gap-10">
+						<img
+							src={match.femaleContestant.headShotUrl}
+							alt={match.femaleContestant.name}
+							class="rounded-md object-cover h-12 w-12"
+						/>
+						<div class="flex gap-2 flex-col">
+							<div class="flex gap-2">
+								<Card.Title>{match.femaleContestant.name}</Card.Title>
+								<Badge class={cn('bg-female hover:bg-female h-5')}>Female</Badge>
+							</div>
+							<Card.Description>{match.femaleContestant.age} years old</Card.Description>
+						</div>
 					</div>
 				</Card.Header>
 			</Card.Root>
