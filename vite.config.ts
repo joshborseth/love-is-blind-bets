da04de3e0c68c63873db3af03969b1b5a79efbcb
joshborseth <joshuaborseth@gmail.com>
2024-10-01 10:37:17 -0700

    specify origin

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,8 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}', 'server/**/*.{test,spec}.{js,ts}']
+	},
+	server: {
+		origin: 'https://luvisblind.app'
 	}
 });
