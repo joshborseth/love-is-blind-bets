19dccd2c75e7f38f2433e088f8aed43b1ced4893
joshborseth <joshuaborseth@gmail.com>
2024-09-30 18:30:07 -0700

    add point system to scoreboard page

--- src/routes/app/scoreboard/+page.svelte
+++ src/routes/app/scoreboard/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import * as Alert from '@/components/ui/alert';
+	import { MARRIAGE_GUESS_POINTS, MATCH_GUESS_POINTS } from '@/constants/points';
 </script>
 
 <div class="flex flex-col gap-2">
@@ -13,6 +14,10 @@
 		<Alert.Title>Points - Coming Soon</Alert.Title>
 		<Alert.Description
 			>We will start tallying up the points once the contestants are matched up in the show!</Alert.Description
+		>
+		<Alert.Description
+			>Correct marriages will earn you {MARRIAGE_GUESS_POINTS} points, while correct matches will earn
+			you {MATCH_GUESS_POINTS} point.</Alert.Description
 		>
 	</Alert.Root>
 </div>
